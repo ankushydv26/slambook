39ff9a25e7fc04135d077019102b45a80dd2c10a
Ankush <_noReply@.com_>
2022-08-20 03:49:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:'flex' , justifyContent:'center' , padding:'06%'}} >
+      <div>
+        <h1>this is the just mind wall</h1>
+        <pre style={{ fontSize :"26px"}}>
+          Where I'm write  my thought 
+             
+             " Everyday getting worst  from yesterday , 
+{/*                             Everyhours  getting  */} 
+        </pre>
+      </div>
     </div>
   );
 }
